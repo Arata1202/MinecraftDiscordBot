b9b9849bd836a46a2f578103928840c8269e7e62
Arata <aratadayoon@yahoo.co.jp>
2025-05-18 12:22:31 +0900

    サーバ起動・停止コマンドの応答処理を簡素化し、エラーハンドリングを改善

--- api/stop.go
+++ api/stop.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"io"
 	"net/http"
 	"os"
 
@@ -16,44 +15,21 @@ func StopCommand() *discordgo.ApplicationCommand {
 }
 
 func StopHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
 	url := os.Getenv("LAMBDA_STOP_URL")
 
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Content: "サーバの停止を開始します",
 		},
 	})
 
-	if err != nil {
-
-		s.FollowupMessageCreate(i.Interaction, false, &discordgo.WebhookParams{
-			Content: "コマンド応答中にエラーが発生しました",
-		})
-		return
-	}
-
 	resp, err := http.Get(url)
 	if err != nil {
-
 		s.FollowupMessageCreate(i.Interaction, false, &discordgo.WebhookParams{
 			Content: "停止処理呼び出し失敗",
 		})
 		return
 	}
 	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-
-		s.FollowupMessageCreate(i.Interaction, false, &discordgo.WebhookParams{
-			Content: "サーバからの応答の読み取りに失敗しました",
-		})
-		return
-	}
-
-	s.FollowupMessageCreate(i.Interaction, false, &discordgo.WebhookParams{
-		Content: "サーバの停止処理が完了しました。応答: " + string(body),
-	})
 }
