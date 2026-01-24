a10641cc89fbbb116952deef30b945e26be516e7
Arata <aratadayoon@yahoo.co.jp>
2026-01-25 00:56:29 +0900

    Terraform設定を更新し、EC2インスタンスのボリューム暗号化とKMSキーIDを追加。変数ファイルを新規作成し、必要な変数を定義。

--- .terraform/ec2/ec2.tf
+++ .terraform/ec2/ec2.tf
@@ -5,8 +5,11 @@ resource "aws_instance" "minecraft_bot_server" {
   vpc_security_group_ids = [aws_security_group.minecraft_bot_sg.id]
 
   root_block_device {
-    volume_type = var.volume_type
-    volume_size = var.volume_size
+    volume_type           = var.volume_type
+    volume_size           = var.volume_size
+    encrypted             = var.volume_encrypted
+    kms_key_id            = var.volume_kms_key_id
+    delete_on_termination = false
   }
 
   tags = {
